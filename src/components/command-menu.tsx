7913cf4154982bcf3a64d2160212d107de9c07c0
Ravi <42783430+rav3-S@users.noreply.github.com>
2024-01-20 20:04:20 +0530

    Initial commit

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -39,10 +39,17 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
+<<<<<<< HEAD
       <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">{isMac ? "⌘" : "Ctrl"}</span>+J
+=======
+      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
+        Press{" "}
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>J
+>>>>>>> 384e77b (Initial commit)
         </kbd>{" "}
         to open the command menu
       </p>
@@ -50,7 +57,11 @@ export const CommandMenu = ({ links }: Props) => {
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
+<<<<<<< HEAD
         className="fixed bottom-4 right-4 flex rounded-full shadow-2xl xl:hidden print:hidden"
+=======
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
+>>>>>>> 384e77b (Initial commit)
       >
         <CommandIcon className="my-6 size-6" />
       </Button>
